39a85c420a2b1ec2730d125b53ae3e9b90bc0484
youta1119 <youta1119ume@gmail.com>
2017-10-12 11:55:40 +0900

    Add appIcon

---
+++ app/src/main/java/team_apple/github/com/gencon/CalenderRecyclerView.kt
@@ -0,0 +1,6 @@
+package team_apple.github.com.gencon
+
+/**
+ * Created by youta on 2017/07/15.
+ */
+class CalenderRecyclerView
---
+++ app/src/main/java/team_apple/github/com/gencon/CalenderView.kt
@@ -0,0 +1,5 @@
+package team_apple.github.com.gencon
+
+/**
+ * Created by youta on 2017/07/15.
+ */
