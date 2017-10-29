088feda0770a211eecc12ec304e7ab448546b7ea
youta1119 <youta1119ume@gmail.com>
2017-10-29 20:37:00 +0900

    Fix something

---
+++ app/src/main/java/com/github/team_apple/gencon/presentation/ui/eventedit/MainActivity.java
@@ -0,0 +1,8 @@
+package com.github.team_apple.gencon.presentation.ui.eventedit;
+
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+
+}
--- app/src/main/java/com/github/team_apple/gencon/presentation/util/BottomNavigationViewBehavior.kt
+++ app/src/main/java/com/github/team_apple/gencon/presentation/util/BottomNavigationViewBehavior.kt
@@ -1,39 +1,35 @@
 package com.github.team_apple.gencon.presentation.util
 
 import android.content.Context
+import android.support.design.widget.AppBarLayout
 import android.support.design.widget.BottomNavigationView
 import android.support.design.widget.CoordinatorLayout
 import android.support.v4.view.ViewCompat
 import android.util.AttributeSet
 import android.view.View
-
 @Suppress("unused")
 class BottomNavigationViewBehavior(context: Context?, attrs: AttributeSet?) : CoordinatorLayout.Behavior<BottomNavigationView>(context, attrs) {
     private var height = 0f
 
-    override fun onStartNestedScroll(coordinatorLayout: CoordinatorLayout,
-                                     child: BottomNavigationView,
-                                     directTargetChild: View,
-                                     target: View,
-                                     nestedScrollAxes: Int): Boolean {
+    override fun onStartNestedScroll(coordinatorLayout: CoordinatorLayout, child: BottomNavigationView,
+                                     directTargetChild: View, target: View, nestedScrollAxes: Int): Boolean {
         return nestedScrollAxes == ViewCompat.SCROLL_AXIS_VERTICAL
     }
 
+    override fun layoutDependsOn(parent: CoordinatorLayout, child: BottomNavigationView, dependency: View): Boolean {
+        return dependency is AppBarLayout.Behavior
+    }
 
-    override fun onLayoutChild(parent: CoordinatorLayout,
-                               child: BottomNavigationView,
+    override fun onLayoutChild(parent: CoordinatorLayout, child: BottomNavigationView,
                                layoutDirection: Int): Boolean {
         height = child.height.toFloat()
         return super.onLayoutChild(parent, child, layoutDirection)
     }
 
-    override fun onNestedScroll(coordinatorLayout: CoordinatorLayout,
-                                child: BottomNavigationView,
-                                target: View,
-                                dxConsumed: Int,
-                                dyConsumed: Int,
-                                dxUnconsumed: Int,
-                                dyUnconsumed: Int) {
+    override fun onNestedScroll(coordinatorLayout: CoordinatorLayout, child: BottomNavigationView,
+                                target: View, dxConsumed: Int, dyConsumed: Int,
+                                dxUnconsumed: Int, dyUnconsumed: Int) {
+
         super.onNestedScroll(coordinatorLayout, child, target, dxConsumed, dyConsumed, dxUnconsumed, dyUnconsumed)
         if (dyConsumed < 0) {
             child.clearAnimation()
@@ -43,16 +39,4 @@ class BottomNavigationViewBehavior(context: Context?, attrs: AttributeSet?) : Co
             child.animate().translationY(height).duration = 200
         }
     }
-
-
-    fun slideDown(child: BottomNavigationView){
-
-    }
-
-    fun slideUp(child: BottomNavigationView){
-        ;
-    }
-
-
-
 }
